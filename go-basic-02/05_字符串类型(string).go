a5cb7123e9c796d5a84c969b8343e0180e6cae3c
hyd <1357644645@qq.com>
2025-08-03 20:06:11 +0800

    提交最新

--- go-basic-02/05_字符串类型(string).go
+++ go-basic-02/05_字符串类型(string).go
@@ -11,7 +11,7 @@ func main() {
 	var str = "ms的go教程 \n 大法好"
 	fmt.Print(str)
 
-	fmt.Println(`\t ms的go教程Go大法好`) // \t ms的go教程Go大法好
+	fmt.Println(`\t ms的go教程Go大法好`)  // \t ms的go教程Go大法好
 	fmt.Println(`\t ms的go教程
  Go大法好`) // 使用反引号 可以进行字符串换行
 	// 反引号一般用在 需要将内容进行原样输出的时候 使用
@@ -19,10 +19,9 @@ func main() {
 	var mystr01 string = "hello"
 	var mystr02 [5]byte = [5]byte{104, 101, 108, 108, 111}
 	fmt.Printf("myStr01: %s\n", mystr01)
-	fmt.Printf("myStr02: %s", mystr02)
+	fmt.Printf("myStr02: %s\n", mystr02)
 
 	// 中文三字节，字母一个字节
 	var myStr01 string = "hello,ms的go教程"
 	fmt.Printf("mystr01: %d\n", len(myStr01))
-
 }
